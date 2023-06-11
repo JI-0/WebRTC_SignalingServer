58c366b6fd0b5e86cda40e9789987d36b0dd0aff
JI-0 <janivkovic11111@gmail.com>
2023-06-11 19:12:07 +0900

    Added message parser

---
+++ messageParser.go
@@ -0,0 +1,13 @@
+package main
+
+import "strings"
+
+func parseMessage(c *Client, msg string) bool {
+	parts := strings.Split(msg, "\n")
+	switch parts[0] {
+
+	}
+
+	// No function match
+	return false
+}
