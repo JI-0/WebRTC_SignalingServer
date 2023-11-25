d511db8f83627acb567b221774620ffe663e6f7e
JI-0 <janivkovic11111@gmail.com>
2023-11-25 22:57:30 +0900

    Fixed second port listening, simplified TLS

--- main.go
+++ main.go
@@ -12,40 +12,31 @@ func main() {
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", manager.serveWS)
+	tlsConf := &tls.Config{
+		GetCertificate: func(*tls.ClientHelloInfo) (*tls.Certificate, error) {
+			cert, err := tls.LoadX509KeyPair("/etc/letsencrypt/live/the.testingwebrtc.com/fullchain.pem",
+				"/etc/letsencrypt/live/the.testingwebrtc.com/privkey.pem")
+			if err != nil {
+				log.Println("Failed to load TLS certificate!")
+				return nil, err
+			}
+			return &cert, nil
+		},
+	}
 
 	srv0 := &http.Server{
-		Addr:    ":3000",
-		Handler: mux,
-		TLSConfig: &tls.Config{
-			GetCertificate: func(*tls.ClientHelloInfo) (*tls.Certificate, error) {
-				cert, err := tls.LoadX509KeyPair("/etc/letsencrypt/live/the.testingwebrtc.com/fullchain.pem",
-					"/etc/letsencrypt/live/the.testingwebrtc.com/privkey.pem")
-				if err != nil {
-					log.Println("Failed to load TLS certificate!")
-					return nil, err
-				}
-				return &cert, nil
-			},
-		},
+		Addr:      ":3000",
+		Handler:   mux,
+		TLSConfig: tlsConf,
 	}
 	srv1 := &http.Server{
-		Addr:    ":3001",
-		Handler: mux,
-		TLSConfig: &tls.Config{
-			GetCertificate: func(*tls.ClientHelloInfo) (*tls.Certificate, error) {
-				cert, err := tls.LoadX509KeyPair("/etc/letsencrypt/live/the.testingwebrtc.com/fullchain.pem",
-					"/etc/letsencrypt/live/the.testingwebrtc.com/privkey.pem")
-				if err != nil {
-					log.Println("Failed to load TLS certificate!")
-					return nil, err
-				}
-				return &cert, nil
-			},
-		},
+		Addr:      ":3001",
+		Handler:   mux,
+		TLSConfig: tlsConf,
 	}
 
-	go log.Fatal(srv0.ListenAndServeTLS("", ""))
-	log.Fatal(srv1.ListenAndServeTLS("", ""))
+	go srv0.ListenAndServeTLS("", "")
+	srv1.ListenAndServeTLS("", "")
 
 	println("Shutting down signaling server")
 }
